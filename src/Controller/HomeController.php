e7584d6bbabcf92e663a4e1bc7511559b48236d6
anthony <delgado.anthony@gmail.com>
2018-11-03 10:42:15 +0100

    hello in homepage from new entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Hello;
 
 /**
  * 
@@ -17,9 +18,10 @@ Class HomeController extends Controller
      */
     public function index(Request $request) 
     {
+        $hellos = $this->getDoctrine()->getRepository(Hello::class)->findAll();
+        $hello = $hellos[array_rand($hellos)];
         return $this->render('pages/home.html.twig',[
-            'hello' => 'hola i benvinguts',
-            'helloLanguage' => 'catalan',
+            'hello' => $hello,
         ]);
     }
 }
